b105f2b7a945b20e6e97856ff86e705eab43ee82
Suresokken <jigzie@gmail.com>
2016-05-11 10:19:14 +0200

    small

--- server/config/seed.js
+++ server/config/seed.js
@@ -2,7 +2,6 @@
  * Populate DB with sample data on server start
  * to disable, edit config/environment/index.js, and set `seedDB: false`
  */
-
 'use strict';
 import Thing from '../api/thing/thing.model';
 import User from '../api/user/user.model';
